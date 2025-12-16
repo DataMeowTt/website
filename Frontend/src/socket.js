38ae609bab8c07ce060004b3e7512faa80843d4a
TuanTuVinhPhuc <trantuann1809@gmail.com>
2025-12-16 21:27:05 +0700

    xoa file up nham

--- Frontend/src/socket.js
+++
@@ -1,8 +0,0 @@
-// src/socket.js
-import { io } from "socket.io-client";
-const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || "http://localhost:3000";
-const socket = io(SOCKET_URL, {
-  transports: ["websocket"],
-  withCredentials: true,
-});
-export default socket;
