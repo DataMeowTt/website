f7a26d642c5bfd970e30fba379a1c729994246c5
TuanTuVinhPhuc <trantuann1809@gmail.com>
2025-12-16 20:49:33 +0700

    add branch Fe

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './styles/global.css';
+
+function App() {
+  return (
+    <Router>
+    </Router>
+  );
+}
+
+export default App;
