da6233388cdbda860045035f194ae7aa5fd33c42
TuanTuVinhPhuc <trantuann1809@gmail.com>
2025-12-16 21:39:56 +0700

    tuan add giao dien co ban

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
 import './styles/global.css';
+
+import { AuthProvider } from './contexts/AuthContext';
+
 
 function App() {
   return (
-    <Router>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Header />
+                <Home />
+                <Footer />
+              </>
+            }
+          />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
