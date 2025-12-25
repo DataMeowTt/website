cd9ebbde4946767ab85928db6e3f5f82c85e17bb
Nguyễn Sơn <169588153+Nostagi@users.noreply.github.com>
2025-12-26 04:49:13 +0700

    Cập nhật FE trang giải đấu + Fix lỗi định tuyến

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,9 +6,19 @@ import Footer from './components/Footer';
 import Home from './pages/Home';
 import './styles/global.css';
 
-import Centers from "./pages/Centers";
-import News from './pages/News';
+import UserProfile from "./pages/UserProfile";
+import Service from "./pages/Service";
+import Competition from "./pages/Competition"
+import Contact from "./pages/Contact";
 import Policy from './pages/Policy';
+import News from './pages/News';
+import Centers from "./pages/Centers";
+import BookingSchedule from './pages/Booking';
+import PaymentPage from './pages/Payment';
+import ResetPasswordPage from "./pages/ResetPassword";
+
+import WeatherDisplay from './components/WeatherDisplay'; 
+import Scroll from './components/Scroll'; 
 
 import { AuthProvider } from './contexts/AuthContext';
 
@@ -17,6 +27,7 @@ function App() {
   return (
     <AuthProvider>
       <Router>
+        <Scroll />
         <Routes>
           <Route
             path="/"
@@ -28,12 +39,18 @@ function App() {
               </>
             }
           />
-          <Route path="/centers" element={<Centers />} />
-          <Route path="/booking" element={<Booking />} />
-          <Route path="/payment" element={<Payment />} />
-          <Route path="/news" element={<News />} />
+          <Route path="/profile" element={<UserProfile />} />
+          <Route path="/service" element={<Service />} />
+          <Route path="/competition" element={<Competition />} />
+          <Route path="/contact" element={<Contact />} />
           <Route path="/policy" element={<Policy />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/centers" element={<Centers />} />
+          <Route path="/booking" element={<BookingSchedule />} />
+          <Route path="/payment" element={<PaymentPage />} />
+          <Route path="/reset-password/:token/:userId" element={<ResetPasswordPage />} />
         </Routes>
+        <WeatherDisplay />
       </Router>
     </AuthProvider>
   );
