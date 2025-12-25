5f54d84cc5c6e70014d10a72cd0fb26f9c1d564f
TuanTuVinhPhuc <trantuann1809@gmail.com>
2025-12-25 22:20:42 +0700

    merge AI

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -3,7 +3,17 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-d
 import Dashboard from "@/pages/Dashboard";
 import Login from "@/pages/Login";
 import News from '@/pages/News';
+import Rating from '@/pages/RatingManagement';
 import Account from '@/pages/Account';
+import Shop from '@/pages/Shop';
+import Stock from '@/pages/stockManagement';
+import Report from '@/pages/Report'
+import UserManage from './pages/UserManage';
+import AdminBillList from './pages/BillManage';
+import CreateFixedBooking from './pages/CreateFixedBooking';
+import CourtStatusPage from './pages/centerStatus';
+
+
 
 function App() {
   const isAuthenticated = false;  // Thay đổi theo logic xác thực thực tế
@@ -15,11 +25,16 @@ function App() {
         <Route path="/" element={<Navigate to={isAuthenticated ? "/dashboard" : "/login"} />} />
         
         <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />  
-
-        <Route path="/news" element={<News />} />           
-
+        <Route path="/dashboard" element={<Dashboard />} />            
+        <Route path="/news" element={<News />} /> 
         <Route path="/account" element={<Account />} />
+        <Route path="/shop" element={<Shop />}/> 
+        <Route path="/stock" element={<Stock />}/> 
+        <Route path="/report" element={<Report />}/> 
+        <Route path="/ratings" element={<Rating />} /> 
+        <Route path="/users-manage" element={<UserManage />} />
+        <Route path="/admin-bill-list" element={<AdminBillList />} />
+        <Route path="/create-fixed-booking" element={<CreateFixedBooking />} />
         
       </Routes>
     </Router>
